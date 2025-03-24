eae364e4f35fe40acf5092aa9fdd7871815180f1
Michael Helgesen <michaelhelgesen02@gmail.com>
2025-03-24 21:50:38 +0100

    Add dialog and form

--- book.js
+++ book.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
-
+const dialogElement = document.getElementById("addBook");
+const dialogTrigger = document.getElementById("dialogTrigger");
 function Book (title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
     this.title = title;
+    this.id = crypto.randomUUID();
     this.author = author;
     this.pages = pages;
     this.read = read;
@@ -35,5 +37,10 @@ const test3 = new Book("test3", "test", 3, false);
 addBookToLibrary(test);
 addBookToLibrary(test2);
 addBookToLibrary(test3);
-console.log(myLibrary);
 displayBooks(myLibrary);
+
+dialogTrigger.addEventListener("click", function() {
+    dialogElement.showModal();
+})
+
+
