0a369bb238e8c0432ea22de5a47c8ee93ff2b114
Michael Helgesen <michaelhelgesen02@gmail.com>
2025-03-26 21:35:58 +0100

    Add method to prototype

--- book.js
+++ book.js
@@ -18,15 +18,12 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.toggleRead = function () {
+}
+
+Book.prototype.toggleRead = function () {
     this.read = !this.read;
   }
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${pages} pages, ${
-      this.read ? "read" : "not read yet"
-    }`;
-  };
-}
+
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
